a240ba447491eef20ef2a8a4c22ae2d2a13f3325
Steffen Uhlig <steffen@familie-uhlig.net>
2021-08-15 19:34:19 +0200

    Remove --verbose and --debug
    Not needed yet

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,6 @@ use clap::{crate_version,AppSettings, Clap};
 #[clap(version = crate_version!())]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
-    #[clap(short, long, parse(from_occurrences))]
-    verbose: i32,
-
     #[clap(subcommand)]
     subcmd: SubCommands,
 }
@@ -26,28 +23,16 @@ struct Pomodoro {
 /// Work with a break
 #[derive(Clap)]
 struct Break {
-    /// Print debug info
-    #[clap(short, long)]
-    debug: bool
 }
 
 fn main() {
     let opts: Opts = Opts::parse();
-
-    match opts.verbose {
-        0 => { /* nothing printed */ },
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        _ => println!("That's verbose enough"),
-    }
 
     match opts.subcmd {
         SubCommands::Pomodoro(_) => {
             println!("Let's do something with a Pomodoro");
         }
         SubCommands::Break(t) => {
-            if t.debug {
-                println!("Printing debug info...");
             }
 
             println!("Let's do something with a break");
