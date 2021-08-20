f6f2bc6806e0fc471a4bd0c3bc2679efd8cc08bc
Steffen Uhlig <steffen@familie-uhlig.net>
2021-08-20 22:30:44 +0200

    Fix insert order

--- src/rustomato/persistence.rs
+++ src/rustomato/persistence.rs
@@ -19,7 +19,7 @@ impl Repository {
             Status::Active => {
                 self.db.execute(
                     "INSERT INTO schedulables (kind, uuid, duration, started_at) VALUES (?1, ?2, ?3,strftime('%s','now'))",
-                    params![s.kind, s.duration, s.uuid.to_simple().to_string()],
+                    params![s.kind, s.uuid.to_simple().to_string(), s.duration],
                 )
                 .expect("Failed to insert schedulable");
             }
